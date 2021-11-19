95598c96edcf59c976ca020732d236df2e7b3ca0
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2021-11-19 18:52:59 -0300

    I can now plot the spectrum of the finite chain for different values of \mu

--- kitaev.py
+++ kitaev.py
@@ -6,19 +6,55 @@ Created on Fri Nov 19 13:24:37 2021
 """
 import kwant
 import numpy as np
+import matplotlib.pyplot as plt
 
-def make_kitaev_chain_infinite(t, mu, Delta):
+#Pauli matrices
+tau_z = np.array([[1, 0],[0, -1]])
+tau_y = np.array([[0, -1j],[1j, 0]])
+tau_x = np.array([[0, 1],[1, 0]])
+
+def make_kitaev_chain_finite(t=1, mu=1, Delta=1, L=25):
     """
-    Create a an infinite translationally invariant Kitaev chain.
+    Create a finite translationally invariant Kitaev chain.
     """
     # Start with an empty tight-binding system. On each site, there
     # are now electron and hole orbitals, so we must specify the
     # number of orbitals per site 'norbs'.
-    kitaev_chain_infinite = kwant.Builder(kwant.TranslationalSymmetry((-1,)))
+    kitaev_chain_finite = kwant.Builder()
     lat = kwant.lattice.chain(norbs=2)  
     # The superconducting order parameter couples electron and hole orbitals
     # on each site, and hence enters as an onsite potential
-    kitaev_chain_infinite[lat(0)] = -mu * np.array([[1, 0], [0, -1]])
+    def onsite(onsite, mu):
+        return -mu * tau_z
+    kitaev_chain_finite[(lat(x) for x in range(L))] = onsite
     # Hoppings
-    kitaev_chain_infinite[kwant.HoppingKind((1,), lat)] = -t * np.array([[1, 0],[0, -1]]) - Delta * np.eye(2)
+    def hop(site1, site2, t, Delta):
+        return -t * tau_z - 1j * Delta * tau_y
+    kitaev_chain_finite[lat.neighbors()] = hop   
+    return kitaev_chain_finite    
+
+#kitaev = make_kitaev_chain_finite().finalized()
+#energies = np.linalg.eigvals(kitaev.hamiltonian_submatrix())
+
+def plot_spectrum(syst, mu):
+    fig = kwant.plotter.spectrum(syst, ("mu", mu), params=dict(t=1, Delta=1))
+    fig.canvas.manager.set_window_title("Bandas de energía")    
+    plt.xlabel(r"$\frac{\mu}{t}$")
+    plt.ylabel(r"Energía")
+
+def main():
+    syst = make_kitaev_chain_finite()
+    # Check that the system looks as intended.
+    #kwant.plot(syst)
+    
+    # Finalize the system.
+    syst = syst.finalized()
+        
+    mu = np.linspace(-4, 4)
+    # We should see the energy bands.
+    plot_spectrum(syst, mu)
+    
+if __name__ == '__main__':
+    main()
+
     
--- readme.rst
+++ readme.rst
@@ -2,3 +2,7 @@ Welcome
 
 This is my readme file for a project on Kitaev chains and their topological properties.
 The idea is to solve the toy model with periodic boundary conditions and with a finite length of the chain.
+
+The tight binding model is simulated with the kwant package.
+The finite chain follows the following course:
+https://topocondmat.org/w1_topointro/1D.html
