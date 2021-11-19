f48ccd8d2bf636302efa6afc66cfeb3d6a022ae9
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2021-11-19 19:32:03 -0300

    I have added docstrings.

--- kitaev.py
+++ kitaev.py
@@ -8,15 +8,30 @@ import kwant
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Pauli matrices
-tau_z = np.array([[1, 0],[0, -1]])
-tau_y = np.array([[0, -1j],[1j, 0]])
-tau_x = np.array([[0, 1],[1, 0]])
-
 def make_kitaev_chain_finite(t=1, mu=1, Delta=1, L=25):
     """
     Create a finite translationally invariant Kitaev chain.
+
+    Parameters
+    ----------
+    t : float, optional
+        Hopping parameter. The default is 1.
+    mu : float optional
+        Chemical potential. The default is 1.
+    Delta : float, optional
+        Superconducting gap. The default is 1.
+    L : int, optional
+        Number of sites in the chain. The default is 25.
+
+    Returns
+    -------
+    kwant.builder.Builder
+        The representation for the Kitaev chain.
+
     """
+    #Pauli matrices
+    tau_z = np.array([[1, 0],[0, -1]])
+    tau_y = np.array([[0, -1j],[1j, 0]])
     # Start with an empty tight-binding system. On each site, there
     # are now electron and hole orbitals, so we must specify the
     # number of orbitals per site 'norbs'.
@@ -33,10 +48,22 @@ def make_kitaev_chain_finite(t=1, mu=1, Delta=1, L=25):
     kitaev_chain_finite[lat.neighbors()] = hop   
     return kitaev_chain_finite    
 
-#kitaev = make_kitaev_chain_finite().finalized()
-#energies = np.linalg.eigvals(kitaev.hamiltonian_submatrix())
-
 def plot_spectrum(syst, mu):
+    """
+    Plot the spectrum by changing the parameter 'mu'.
+
+    Parameters
+    ----------
+    syst : kwant.builder.FiniteSystem
+        Finite Kitaev chain.
+    mu : np.array
+        Values for the chemical potential.
+
+    Returns
+    -------
+    fig : plt.figure
+
+    """
     fig = kwant.plotter.spectrum(syst, ("mu", mu), params=dict(t=1, Delta=1))
     fig.canvas.manager.set_window_title("Bandas de energía")    
     plt.xlabel(r"$\frac{\mu}{t}$")
@@ -56,5 +83,4 @@ def main():
     
 if __name__ == '__main__':
     main()
-
     
