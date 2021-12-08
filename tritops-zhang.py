289d13e521a049c0acfa4567692c503b637f4cf8
Gabriel Rodríguez Ruiz <gabrielrr96@gmail.com>
2021-12-08 10:54:14 -0300

    Added 2D ribbon graphs.

--- tritops-zhang.py
+++ tritops-zhang.py
@@ -28,6 +28,10 @@ def hopping(site1, site2, t, lambda_R, Delta_1):     #np.kron() is the tensor pr
     return (-t * np.kron(sigma_z, np.eye(2)) - lambda_R * np.kron(sigma_z, 1j*sigma_y)
             - Delta_1 * np.kron(sigma_y, sigma_y))
 
+def hopping_y(site1, site2, t, lambda_R, Delta_1):     #np.kron() is the tensor product
+    return (-t * np.kron(sigma_z, np.eye(2)) + lambda_R * np.kron(1j*np.eye(2), sigma_x)
+            - Delta_1 * np.kron(sigma_y, sigma_y))
+
 def make_low_energy_model_finite(mu=0, t=10, lambda_R=5, Delta_0=0, Delta_1=2, L=25):
     """
     Minimal low energy level for TRITOPS based on [Zhang] equation 1
@@ -135,6 +139,46 @@ def update(value):
     fig.canvas.draw()
     fig.canvas.flush_events()
 
+def make_low_energy_model_ribbon(mu=0, t=10, lambda_R=5, Delta_0=0, Delta_1=2, L=25):
+    """
+    Minimal low energy level for TRITOPS based on [Zhang] equation 1 for ribbon of width L.
+    A factor 1/2 is left out. H=1/2 psi* H_BdG psi
+
+    Parameters
+    ----------
+    mu : float
+        Chemical potential.
+    t : float
+        Hopping parameter.
+    lambda_R : float
+        Rashba spin-orbit coupling.
+    Delta_0 : float
+        On-site paring potential.
+    Delta_1 : float
+        Nearest neighbor pairing potential.
+    L : float
+        Width of the ribbon.
+    
+    Returns
+    -------
+    ribbon : kwant.builder.Builder
+        The representation for the ribbon model.
+        
+    """
+    #Create a 2D template
+    sym = kwant.TranslationalSymmetry((1,0), (0,1))
+    syst = kwant.Builder(sym)
+    lat = kwant.lattice.square(1, norbs=4)
+    syst[lat(0,0)] = onsite
+    syst[kwant.HoppingKind((1, 0), lat)] = hopping
+    syst[kwant.HoppingKind((0, 1), lat)] = hopping_y
+    #Fill the target ribbon inside the template syst
+    ribbon = kwant.Builder(kwant.TranslationalSymmetry([1,0]))
+    ribbon.fill(syst, shape=(lambda site: 0 <= site.pos[1] < L), start=[0, 0])
+    #syst[kwant.HoppingKind((1, 0), lat)] = hopping
+    #syst[lat.neighbors()] = hopping 
+    return ribbon
+
 def main():
     global tritops_infinite, tritops_finite, Rashba_slider, fig, mu_slider
     
@@ -149,7 +193,7 @@ def main():
     # We should see the energy bands.
     params = dict(t=10, lambda_R=5, Delta_0=0, Delta_1=2)
     fig = plot_spectrum(tritops_finite, mu, params=params)
-    fig.savefig("../Images/Bandas de un tritops finito.png")
+    fig.savefig("../Images/Bandas de un tritops 2D finito.png")
     
     tritops_infinite = make_low_energy_model_infinite()
     tritops_infinite = tritops_infinite.finalized()
@@ -164,9 +208,34 @@ def main():
     #plt.subplots_adjust(left=0.25, bottom=0.25)
     ax_mu = fig.add_axes([0.25, 0.15, 0.65, 0.03])
     mu_slider = Slider(ax=ax_mu, label=r'$\mu$', valmin=-10, valmax=10, valinit=0)
-    fig.savefig("../Images/Bandas de un tritops infinito.png")
+    fig.savefig("../Images/Bandas de un tritops 2D infinito.png")
 
+def main_2D():
+    
+    tritops_ribbon = make_low_energy_model_ribbon(L=100)
+    # Check that the system looks as intended.
+    #kwant.plot(syst)
+    
+    # Finalize the system.
+    tritops_ribbon = tritops_ribbon.finalized()
+        
+    # We should see the energy bands.
+    params = dict(mu=-10, t=10, lambda_R=5, Delta_0=-2, Delta_1=2)
+    #mu=-10 si Delta_0=-2
+    #mu=10 si Delta_0=2
+    fig = kwant.plotter.bands(tritops_ribbon, momenta=1000, params=params)
+    #fig.savefig("../Images/Bandas de una cinta tritops Delta0=-2.png")
+    plt.title(f"Cinta con mu={params['mu']}, t={params['t']}, lambda_R={params['lambda_R']}, Delta_0={params['Delta_0']}, Delta_1={params['Delta_1']}")
+    plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
+               [r"$-\pi$", r"$-\frac{\pi}{2}$", "0", r"$\frac{\pi}{2}$", "$\pi$"])
+    Delta_1 = params["Delta_1"]
+    t = params["t"]
+    plt.yticks([-t, -2*Delta_1, -Delta_1, 0, Delta_1, 2*Delta_1, t], ["-t", r"$-2\Delta_1$", r"$-\Delta_1$",
+                                   0 ,r"$\Delta_1$", r"$2\Delta_1$", "t"])
+    plt.ylim((-t, t))
+    
 if __name__ == '__main__':
-    main()
-    Rashba_slider.on_changed(update)
-    mu_slider.on_changed(update)
+    #main()
+    main_2D()
+    #Rashba_slider.on_changed(update)
+    #mu_slider.on_changed(update)
